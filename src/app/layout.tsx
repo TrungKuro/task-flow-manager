83e8d543989d67091c375e42a7fa30f077b6d691
Trung Hoang <hdh.trung96@gmail.com>
2025-08-30 02:49:31 +0700

    Set font 'Inter' for entire web

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
 import DashboardWrapper from "../components/layouts/DashboardWrapper";
 
 /* ------------------------------------------------------------------------- */
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,9 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={cn(inter.className, "antialiased")}>
         <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
