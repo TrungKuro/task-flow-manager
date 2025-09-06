39988789889723664ddac02368f236b3ff895642
Trung Hoang <hdh.trung96@gmail.com>
2025-09-06 15:05:00 +0700

    Home Page with Charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-export default function HomePage() {
-  return (
-    <div className="flex h-full w-full items-center justify-center bg-red-200">
-      <p className="bg-amber-400 dark:bg-blue-400">Hello World!</p>
-    </div>
-  );
+import HomePage from "./home/page";
+
+export default function AppPage() {
+  return <HomePage />;
 }
